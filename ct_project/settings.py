11e7d905e9c8af3ccf45687ed9b8d861b723ec54
Chris West <cjswest01@gmail.com>
2022-01-23 16:34:14 +0000

    added environment setting to settings.py

--- ct_project/settings.py
+++ ct_project/settings.py
@@ -9,14 +9,17 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import environ
+
 import os
 from pathlib import Path
 
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
+if ENVIRONMENT == 'development':
+    import environ
+    # Initialise environment variables
+    env = environ.Env()
+    environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
