0163895c359e4f54426a7f19fa2e0ef3ac11b899
Chris West <cjswest01@gmail.com>
2022-01-23 12:50:06 +0000

    runserver to work with Azure Blob

--- ct_project/settings.py
+++ ct_project/settings.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import environ
 import os
 from pathlib import Path
+
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
